cf190590e7aeaf9ac767d9fa64a784a05f9c433c
draky <draky@foxmail.com>
2025-12-02 17:56:25 +0800

    Toast Notifications

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import router from '@/router';
 import { reactive } from 'vue';
+import { useToast } from 'vue-toastification';
 import axios from 'axios';
 
 
@@ -17,6 +18,8 @@ const form = reactive({
     contactPhone: ''
   }
 });
+
+const toast = useToast();
 
 const handleSubmit = async() => {
   const newJob = {
@@ -35,11 +38,12 @@ const handleSubmit = async() => {
 
   try {
     const response = await axios.post('/api/jobs', newJob);
-    // @todo - show toast
+    toast.success('Job Added Successfully');
     router.push(`/jobs/${response.data.id}`);
 
   } catch (error) {
     console.error('Error fetching job', error);
+    toast.error('Job Was Not Added');
   } 
 };
 
